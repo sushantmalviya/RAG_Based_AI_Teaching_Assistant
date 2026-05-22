0996289c0c22f3f2409d5a104891b387cff1a316
sushantmalviya <sushantmalviya@users.noreply.github.com>
2026-05-22 17:39:13 +0530

    Answer user questions using similar subtitle chunks

--- process_incoming.py
+++ process_incoming.py
@@ -15,20 +15,50 @@ def create_embedding(text_list):
 
     return embedding
 
+def inference(prompt):
+    r = requests.post("http://localhost:11434/api/generate", json={
+        "model": "llama3.2",
+        "prompt": prompt,
+        "stream": False
+    })
+
+    response = r.json()
+    # print(response)
+    return response
+
 
 df = joblib.load("embeddings.joblib")
 
-df1 = df[:10]
 incoming_query = input("Ask your question:")
 question_embedding = create_embedding(incoming_query)[0]
 
-print(question_embedding)
+# print(question_embedding)
 
-similarities = cosine_similarity(np.vstack(df1['embedding']), [question_embedding]).flatten()
-print(similarities)
-top_results = 3
+similarities = cosine_similarity(np.vstack(df['embedding']), [question_embedding]).flatten()
+# print(similarities)
+top_results = 5
 max_idx = similarities.argsort()[::-1][0:top_results]
-print(max_idx)
+# print(max_idx)
 
-new_df = df1.loc[max_idx]
-print(new_df[["title", "number", "text"]])
+new_df = df.loc[max_idx]
+# print(new_df[["title", "number", "text"]])
+
+# for index, item in new_df.iterrows():
+#     print(index, item["title"], item["number"], item["text"], item["start"], item["end"])
+
+prompt = f'''I am teaching sorting algorithms. Here are video subtitle chunks containing video title, video number, start time in seconds, end time in seconds, the text at that time:
+
+{new_df[["title", "number", "start", "end", "text"]].to_json(orient="records")}
+---------------------------------
+"{incoming_query}"
+User asked this question related to the video chunks, you have to answer in a human way (dont mention the above format, its just for you) where and how much content is taught in which video (in which video and at what timestamp) and guide the user to go to that particular video. If user asks unrelated question, tell him that you can only answer questions related to the course
+'''
+
+with open("prompt.txt", "w") as f:
+    f.write(prompt)
+
+response = inference(prompt)["response"]
+print(response)
+
+with open("response.txt", "w") as f:
+    f.write(response)
